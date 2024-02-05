ee1141d453b16f02c4376b2cdea4160ef8b2c939
Samuel-Ricco <samuelricco@gmail.com>
2024-02-05 16:20:45 +0100

    Add project files.

---
+++ BookstoreAPI/Models/Books.cs
@@ -0,0 +1,13 @@
+﻿namespace BookstoreAPI.Models
+{
+    public class Books
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Author { get; set; }
+        public int ReleaseYear { get; set; }
+        public bool IsAvailabe { get; set; }
+    }
+}
+
